25f7d489b13f7d29827e2d23ccbd8162c54654c9
Svitlana <rsv0011@gmail.com>
2024-10-21 20:08:00 +0200

    feat: implement the DI for mock data'

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,7 +1,18 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// One instance per HTTP request, for example shopping cart service that needs to store items only for the duration of one request.
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+// A new instance every time the service is used, for example logging service that logs messages and doesn’t need to keep track of past logs.
+// builder.Services.AddTransient
+//  One instance for the entire app. One instance of the service is created when the app starts, and it’s shared everywhere.
+// builder.Services.AddSingleton
 
 var app = builder.Build();
 
