6066336ba3c5a33b43304ea311a141e499cecc89
Svitlana <rsv0011@gmail.com>
2024-10-22 20:19:28 +0200

    feat: add the details page by id

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -36,5 +36,12 @@ namespace BethanysPieShop.Controllers
             return View(pieListViewModel);
         }
 
+          public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null) return NotFound();
+            return View(pie);
+        }
+
     }
 }
